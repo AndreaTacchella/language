bd848110d5705442f8146d3fb899098f5d24e09a
Andrea Tacchella <andrea.tacchella.ext@lastminute.com>
2018-01-22 19:04:45 +0100

    fixed size random string generator

---
+++ LSTM/lstm.py
@@ -0,0 +1,47 @@
+from torch import nn
+from torch.autograd import Variable
+import torch
+
+class LSTMmodel(nn.Module):
+    def __init__(self, input_s, hidden_s):
+        super(LSTMmodel, self).__init__()
+        self.n_layers = 2
+        self.hidden_size = hidden_s
+        self.input_size = input_s
+        self.lstm = nn.LSTM(input_size=self.hidden_size, hidden_size=self.hidden_size,
+                            num_layers=self.n_layers, dropout=0.5)
+        self.linear = nn.Linear(self.hidden_size, self.input_size)
+        self.embedding = nn.Linear(self.input_size, self.hidden_size)
+        self.logsoft = nn.LogSoftmax()
+        self.optimizer = 0.
+        self.loss_func =  nn.NLLLoss()
+
+    def forward(self, inp):
+        h0 = Variable(torch.randn(self.n_layers, 1, self.hidden_size))
+        c0 = Variable(torch.randn(self.n_layers, 1, self.hidden_size))
+        true_inp = []
+        for i in range(len(inp)):
+            true_inp.append(self.embedding(inp[i]))
+        true_inp = torch.stack(true_inp)
+        out, h0 = self.lstm(true_inp, (h0, c0))
+        out = out.view(-1, self.hidden_size)
+        true_out = []
+        # print 'len inp', len(inp)
+        for i in range(len(inp)):
+            true_out.append(self.logsoft(self.linear(out[i])))
+        # print len(true_out)
+        # print true_out[0].size()
+        return torch.stack(true_out)
+
+    def train(self, mode=True):
+        self.zero_grad()
+        loss = 0
+
+
+
+
+
+
+    def init_hidden(self):
+        return (Variable(torch.randn(1, self.hidden_size)),
+                Variable(torch.randn(1, self.hidden_size)))
--- TextualData/TextualData.py
+++ TextualData/TextualData.py
@@ -1,5 +1,7 @@
 from io import open
 import numpy as np
+from torch.autograd import Variable
+import torch
 
 class TextualData():
     def __init__(self, path):
@@ -9,28 +11,31 @@ class TextualData():
         self.letter_to_ix = {}
         self.__import_text_file(path)
         self.text_len = len(self.full_text)
+        self.vocab_len = 0
+        self.word_to_ix = {}
+
 
 
     def string_to_tensor(self, string):
         my_str = string
         #my_str += '/'
-        ret = Variable(torch.zeros(len(my_str), 1, alpha_len))
+        ret = Variable(torch.zeros(len(my_str), 1, self.alpha_len))
         for i in range(len(my_str)):
-            ret[i,0,letter_to_ix[my_str[i]]] = 1
+            ret[i,0,self.letter_to_ix[my_str[i]]] = 1
         return ret
 
     def words_to_tensor(self, string):
         my_str = string
         #my_str += '/'
-        ret = Variable(torch.zeros(len(my_str), 1, vocab_len))
+        ret = Variable(torch.zeros(len(my_str), 1, self.vocab_len))
         for i in range(len(my_str)):
-            if (my_str[i] in word_to_ix.keys()):
-                ret[i,0,word_to_ix[my_str[i]]] = 1
+            if (my_str[i] in self.word_to_ix.keys()):
+                ret[i,0,self.word_to_ix[my_str[i]]] = 1
             else:
-                ret[i,0,word_to_ix['UNK']] = 1
+                ret[i,0,self.word_to_ix['UNK']] = 1
         return ret
 
-    def random_string(self, rnd=-1, batch_len):
+    def random_string(self, batch_len, rnd=-1):
         if rnd < 0:
             my_rnd = np.random.randint(self.text_len-batch_len)
         else:
@@ -42,6 +47,17 @@ class TextualData():
             k = k + 1
         input_string = self.full_text[my_rnd:my_rnd + batch_len + k]
         return input_string
+
+    def random_string_fixed_size(self, batch_len, rnd=-1):
+        if rnd < 0:
+            my_rnd = np.random.randint(self.text_len - batch_len)
+        else:
+            my_rnd = rnd
+        input_string = self.full_text[my_rnd:my_rnd + batch_len]
+        return input_string
+
+    def get_batch(self, string_len, batch_size):
+        return batch
 
     def __import_text_file(self, path):
         self.full_text = open(path, encoding="utf-8").read()
